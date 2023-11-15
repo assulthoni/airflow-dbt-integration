4472af4c2cfc4526fed1297ff3a654287db42dc7
thonipintu <ahmad.shohibus@pintu.co.id>
2023-11-15 21:34:32 +0700

    add changes

--- dags/dbt_dag_advance.py
+++ dags/dbt_dag_advance.py
@@ -31,7 +31,7 @@ def make_dbt_task(node, dbt_verb):
             task_id=node,
             bash_command=(
                 f"cd {DBT_DIR} && "
-                f"dbt {dbt_verb} --target dev --models {model} "
+                f"dbt {dbt_verb} --models {model} "
                 f"--profiles-dir ."
             ),
         )
@@ -42,12 +42,22 @@ def make_dbt_task(node, dbt_verb):
             task_id=node_test,
             bash_command=(
                 f"cd {DBT_DIR} && "
-                f"dbt {dbt_verb} --target dev --models {model} "
+                f"dbt {dbt_verb} --models {model} "
                 f"--profiles-dir ."
             ),
         )
     return dbt_task
 
+
+dbt_compile = BashOperator(
+    dag=dag,
+    task_id='dbt_compile',
+    bash_command=(
+        f"cd {DBT_DIR} && "
+        f"dbt compile "
+        f"--profiles-dir ."
+    ),
+)
 
 data = load_manifest()
 dbt_tasks = {}
@@ -61,8 +71,8 @@ for node in data["nodes"].keys():
 for node in data["nodes"].keys():
     if node.split(".")[0] == "model":
         node_test = node.replace("model", "test")
-        dbt_tasks[node] >> dbt_tasks[node_test]
+        dbt_compile >> dbt_tasks[node] >> dbt_tasks[node_test]
         for upstream_node in data["nodes"][node]["depends_on"]["nodes"]:
             upstream_node_type = upstream_node.split(".")[0]
             if upstream_node_type == "model":
-                dbt_tasks[upstream_node] >> dbt_tasks[node]
+                dbt_compile >> dbt_tasks[upstream_node] >> dbt_tasks[node]
